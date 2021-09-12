c8756c251ea6b303d0b738bc3cf88ee886298044
Felix Razykov <frazykov@gmail.com>
2021-09-12 13:45:03 +0300

    Add connect, list issues and comment funcs

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,90 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "linear" is now active!');
+import {
+  init,
+  storeApiKey,
+  getMyIssues,
+  setContextIssueId,
+  addContextIssueComment,
+} from "./linear";
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('linear.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Linear!');
-	});
+// This method is called when the extension is activated.
+// The extension is activated the very first time the command is executed.
+export async function activate(context: vscode.ExtensionContext) {
+  const hasApiKey = await init(context);
 
-	context.subscriptions.push(disposable);
+  if (!hasApiKey) {
+    vscode.window.showInformationMessage(
+      'Please run "Connect to Linear" to initialize the connection.'
+    );
+  } else {
+    vscode.window.showInformationMessage(
+      "Your Linear client connection is all set!"
+    );
+  }
+
+  // Commands have been defined in the package.json file
+  // The commandId parameter must match the command field in package.json
+
+  const connectDisposable = vscode.commands.registerCommand(
+    "linear.connect",
+    async () => {
+      const apiKey = (await vscode.window.showInputBox())?.toString();
+
+      if (apiKey) {
+        await storeApiKey(apiKey);
+        vscode.window.showInformationMessage(
+          "Your Linear client connection is all set!"
+        );
+      } else {
+        vscode.window.showErrorMessage("No API key - no cookies!");
+      }
+    }
+  );
+  context.subscriptions.push(connectDisposable);
+
+  const getMyIssuesDisposable = vscode.commands.registerCommand(
+    "linear.getMyIssues",
+    async () => {
+      const issues = await getMyIssues();
+      const selectedIssue = await vscode.window.showQuickPick(
+        issues?.map((issue) => ({
+          label: issue.identifier,
+          description: issue.title,
+          target: issue.id,
+        })) || [],
+        {
+          placeHolder: "Select an issue to save to the working context",
+        }
+      );
+      if (selectedIssue) {
+        setContextIssueId(selectedIssue.target);
+        vscode.window.showInformationMessage(
+          `Linear context issue is set to ${selectedIssue.label}.`
+        );
+      }
+    }
+  );
+  context.subscriptions.push(getMyIssuesDisposable);
+
+  const addContextIssueCommentDisposable = vscode.commands.registerCommand(
+    "linear.addContextIssueComment",
+    async () => {
+      const comment = (await vscode.window.showInputBox())?.toString();
+
+      if (comment) {
+        if (await addContextIssueComment(comment)) {
+          vscode.window.showInformationMessage("Context issue comment added!");
+        } else {
+          vscode.window.showErrorMessage("Error commenting the context issue.");
+        }
+      } else {
+        vscode.window.showErrorMessage("Comment cannot be empty.");
+      }
+    }
+  );
+  context.subscriptions.push(addContextIssueCommentDisposable);
 }
 
-// this method is called when your extension is deactivated
+// This method is called when your extension is deactivated
 export function deactivate() {}
---
+++ src/linear.ts
@@ -0,0 +1,84 @@
+import { ExtensionContext, SecretStorage, Memento } from "vscode";
+import { Issue, LinearClient } from "@linear/sdk";
+
+let _secretStorage: SecretStorage;
+let _storage: Memento;
+let _apiKey: string | undefined;
+let _client: LinearClient | null = null;
+
+export const init = async (context: ExtensionContext): Promise<boolean> => {
+  _secretStorage = context.secrets;
+  _storage = context.workspaceState;
+  try {
+    _apiKey = (await _secretStorage.get("apiKey"))?.toString();
+    _client = new LinearClient({
+      apiKey: _apiKey,
+    });
+  } catch (err) {
+    console.error("No API key stored", err);
+  }
+  return !!_apiKey;
+};
+
+export const storeApiKey = async (apiKey: string): Promise<boolean> => {
+  try {
+    await _secretStorage.store("apiKey", apiKey);
+  } catch (err) {
+    console.error("Error storing API key", err);
+    return false;
+  }
+  return true;
+};
+
+export const getMyIssues = async (): Promise<Issue[] | null> => {
+  if (_client) {
+    const me = await _client.viewer;
+    const myIssues = await me.assignedIssues();
+
+    return myIssues.nodes;
+  } else {
+    console.error("No initialized Linear client found");
+    return null;
+  }
+};
+
+export const setContextIssueId = async (issueId?: string): Promise<boolean> => {
+  if (!issueId) {
+    return false;
+  }
+  try {
+    await _storage.update("linearContextIssueId", issueId);
+  } catch (err) {
+    console.error("Error setting context issue", err);
+    return false;
+  }
+  return true;
+};
+
+export const addContextIssueComment = async (
+  comment: string
+): Promise<boolean> => {
+  if (!_client) {
+    return false;
+  }
+  if (!comment) {
+    return false;
+  }
+  try {
+    const issueId = (await _storage.get("linearContextIssueId")) as string;
+    if (!issueId) {
+      return false;
+    }
+    const commentPayload = await _client.commentCreate({
+      issueId,
+      body: comment,
+    });
+    if (!commentPayload.success) {
+      return false;
+    }
+  } catch (err) {
+    console.error("Error commenting context issue", err);
+    return false;
+  }
+  return true;
+};
